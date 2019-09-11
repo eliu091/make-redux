b3054e2cc4d1abe52fe6a41ec466986a785755b2
Eric Liu <eric.liu@pwc.com>
2019-09-11 18:55:54 +0800

    001

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const appState = {
+  title: {
+    text: 'Test Titile',
+    color: 'red',
+  },
+  content: {
+    text: 'Test Content',
+    color: 'blue'
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function renderApp (appState) {
+    renderTitle(appState.title)
+    renderContent(appState.content)
+  }
+  
+  function renderTitle (title) {
+    const titleDOM = document.getElementById('title')
+    titleDOM.innerHTML = title.text
+    titleDOM.style.color = title.color
+  }
+  
+  function renderContent (content) {
+    const contentDOM = document.getElementById('content')
+    contentDOM.innerHTML = content.text
+    contentDOM.style.color = content.color
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  renderApp(appState)
