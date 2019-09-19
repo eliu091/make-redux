bb8697cf9fa527769d5dd08ab8f566b130df7776
Eric Liu <eric.liu@pwc.com>
2019-09-19 11:27:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,108 +1,21 @@
-/*现在我们把它们集中到一个地方，给这个地方起个名字叫做 store，然后构建一个函数 createStore，
-用来专门生产这种 state 和 dispatch 的集合，这样别的 App 也可以用这种模式了*/
-
-// function createStore (state, stateChanger) {
-//   const getState = () => state
-//   const dispatch = (action) => stateChanger(state, action)
-//   return { getState, dispatch }
-// }
-
-/*
-
-createStore 接受两个参数，一个是表示应用程序状态的 state；
-另外一个是 stateChanger，它来描述应用程序状态会根据 action 发生什么变化，其实就是相当于本节开头的 dispatch 代码里面的内容。
-
-createStore 会返回一个对象，这个对象包含两个方法 getState 和 dispatch。
-getState 用于获取 state 数据，其实就是简单地把 state 参数返回。
-dispatch 用于修改数据，和以前一样会接受 action，然后它会把 state 和 action 一并传给 stateChanger，
-那么 stateChanger 就可以根据 action 来修改 state 了。
-
-*/
-
-//修改上一节的dispatch方法成为stateChanger
-// function stateChanger (state, action) {
-//   switch (action.type) {
-//     case 'UPDATE_TITLE_TEXT':
-//       state.title.text = action.text
-//       break
-//     case 'UPDATE_TITLE_COLOR':
-//       state.title.color = action.color
-//       break
-//     default:
-//       break
-//   }
-// }
-
-/*
-针对每个不同的 App，我们可以给 createStore 传入初始的数据 appState，和一个描述数据变化的函数 stateChanger，然后生成一个 store。
-需要修改数据的时候通过 store.dispatch，需要获取数据的时候通过 store.getState。
-*/
-
-/* 
-
-上面的代码有一个问题，我们每次通过 dispatch 修改数据的时候，其实只是数据发生了变化，
-如果我们不手动调用 renderApp，页面上的内容是不会发生变化的。
-但是我们总不能每次 dispatch 的时候都手动调用一下 renderApp，
-我们肯定希望数据变化的时候程序能够智能一点地自动重新渲染数据，而不是手动调用。
-
-往 dispatch里面加 renderApp 就好了，但是这样 createStore 就不够通用了。
-我们希望用一种通用的方式“监听”数据变化，然后重新渲染页面，这里要用到观察者模式。修改 createStore
-
-*/
-
-// function createStore (state, stateChanger) {
-//   const listeners = []
-//   const subscribe = (listener) => listeners.push(listener)
-//   const getState = () => state
-//   const dispatch = (action) => {
-//     stateChanger(state, action)
-//     listeners.forEach((listener) => listener())
-//   }
-//   return { getState, dispatch, subscribe }
-// }
-
-/*
-
-我们在 createStore 里面定义了一个数组 listeners，还有一个新的方法 subscribe，
-可以通过 store.subscribe(listener) 的方式给 subscribe 传入一个监听函数，这个函数会被 push 到数组当中。
-
-我们修改了 dispatch，每次当它被调用的时候，除了会调用 stateChanger 进行数据的修改，
-还会遍历 listeners 数组里面的函数，然后一个个地去调用。相当于我们可以通过 subscribe 传入数据变化的监听函数，
-每当 dispatch 的时候，监听函数就会被调用，这样我们就可以在每当数据变化时候进行重新渲染：
-
-*/
+//经过了这么多节的优化，我们有了一个很通用的 createStore：
 
 function createStore (state, stateChanger) {
   const listeners = []
   const subscribe = (listener) => listeners.push(listener)
   const getState = () => state
   const dispatch = (action) => {
-    stateChanger(state, action)
+    state = stateChanger(state, action) // 覆盖原对象
     listeners.forEach((listener) => listener())
   }
   return { getState, dispatch, subscribe }
 }
 
-function renderApp (appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
-}
-
-function renderTitle (title) {
-  const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
-}
-
-function renderContent (content) {
-  const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
-}
+//它的使用方式是：
 
 let appState = {
   title: {
-    text: 'This is Title',
+    text: 'This it Title',
     color: 'red',
   },
   content: {
@@ -114,19 +27,127 @@ let appState = {
 function stateChanger (state, action) {
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
-      state.title.text = action.text
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      state.title.color = action.color
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
     default:
-      break
+      return state
   }
 }
 
 const store = createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState())) // 监听数据变化
 
-renderApp(store.getState()) // 首次渲染页面
-store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《Updated Title》' }) // 修改标题文本
-store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
+//我们再优化一下，其实 appState 和 stateChanger 可以合并到一起去：
+
+function stateChanger (state, action) {
+  if (!state) {
+    return {
+      title: {
+        text: 'React.js 小书',
+        color: 'red',
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    }
+  }
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state
+  }
+}
+//stateChanger 现在既充当了获取初始化数据的功能，也充当了生成更新数据的功能。
+//如果有传入 state 就生成更新数据，否则就是初始化数据。
+//这样我们可以优化 createStore 成一个参数，因为 state 和 stateChanger 合并到一起了：
+
+function createStore (stateChanger) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = stateChanger(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}) // 初始化 state
+  return { getState, dispatch, subscribe }
+}
+
+//createStore 内部的 state 不再通过参数传入，而是一个局部变量 let state = null。
+//createStore 的最后会手动调用一次 dispatch({})，dispatch 内部会调用 stateChanger，
+//这时候的 state 是 null，所以这次的 dispatch 其实就是初始化数据了。
+//createStore 内部第一次的 dispatch 导致 state 初始化完成，后续外部的 dispatch 就是修改数据的行为了。
+
+//我们给 stateChanger 这个玩意起一个通用的名字：reducer，不要问为什么，它就是个名字而已，修改 createStore 的参数名字：
+
+function createStore (reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}) // 初始化 state
+  return { getState, dispatch, subscribe }
+}
+
+//这是一个最终形态的 createStore，它接受的参数叫 reducer，reducer 是一个函数，细心的朋友会发现，它其实是一个纯函数（Pure Function）。
+
+//如果没有传入 state 或者 state 是 null，那么它就会返回一个初始化的数据。
+//如果有传入 state 的话，就会根据 action 来“修改“数据，但其实它没有、也规定不能修改 state，
+//而是要通过上节所说的把修改路径的对象都复制一遍，然后产生一个新的对象返回。
+//如果它不能识别你的 action，它就不会产生新的数据，而是（在 default 内部）把 state 原封不动地返回。
+
+//reducer 是不允许有副作用的。你不能在里面操作 DOM，也不能发 Ajax 请求，更不能直接修改 state，
+//它要做的仅仅是 —— 初始化和计算新的 state。
+
+//现在我们可以用这个 createStore 来构建不同的 store 了，只要给它传入符合上述的定义的 reducer 即可：
+
+function themeReducer (state, action) {
+  if (!state) return {
+    themeName: 'Red Theme',
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'UPATE_THEME_NAME':
+      return { ...state, themeName: action.themeName }
+    case 'UPATE_THEME_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
+...
